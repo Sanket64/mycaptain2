b856dfb41b4627efacd72083f769afaf9728b7d8
Sanket64 <91771744+Sanket64@users.noreply.github.com>
2021-10-15 12:48:42 +0530

    Add files via upload

---
+++ project4_mycaptain.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+print("Positive numbers from list1")
+
+for i in list1:
+    if i>0:
+        print(i)
+
+list2=[12,14,-95,3]
+print("positive numebrs form list2")
+for j in list2:
+    if j>0:
+        print(j)
